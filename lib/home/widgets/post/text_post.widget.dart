1417e76d40b24d2dcd21442d104e63e91708994b
Aiken Tine Ahac <ahac.aiken@gmail.com>
2023-01-31 09:31:12 +0100

    Actions on text post

--- lib/home/widgets/post/text_post.widget.dart
+++ lib/home/widgets/post/text_post.widget.dart
@@ -3,6 +3,7 @@ import 'package:app/profile/api/models/profile_picture.response.dart';
 import 'package:app/utils/assets.util.dart';
 import 'package:flutter/material.dart';
 import 'package:app/home/api/models/post.response.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 
 class TextPost extends StatelessWidget {
   const TextPost({super.key, required this.post});
@@ -24,18 +25,21 @@ class TextPost extends StatelessWidget {
         borderRadius: BorderRadius.circular(AppAssets.styles.borderRadius),
         color: AppAssets.colors.darkHighlight,
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            PostAuthor(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: PostAuthor(
               pfp: assignProfilePicture(post.author.profilePicture),
               name: '${post.author.firstName} ${post.author.lastName}',
               role: '${post.author.cult?.role}',
             ),
-            const SizedBox(height: 10.0),
-            Text(
+          ),
+          const SizedBox(height: 10.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Text(
               '${post.title}',
               style: TextStyle(
                 fontSize: 24.0,
@@ -43,8 +47,63 @@ class TextPost extends StatelessWidget {
                 color: AppAssets.colors.light,
               ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () => print('upvote'),
+                    icon: Icon(
+                      IconlyBold.arrowUp,
+                      color: AppAssets.colors.light,
+                    ),
+                  ),
+                  Text(
+                    post.score.toString(),
+                    style: TextStyle(
+                      color: AppAssets.colors.light,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () => print('downvote'),
+                    icon: Icon(
+                      IconlyBold.arrowDown,
+                      color: AppAssets.colors.light,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () => print('comment'),
+                    icon: Icon(
+                      IconlyBold.chat,
+                      color: AppAssets.colors.light,
+                    ),
+                  ),
+                  Text(
+                    '20',
+                    style: TextStyle(
+                      color: AppAssets.colors.light,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              IconButton(
+                onPressed: () => print('share'),
+                icon: Icon(
+                  IconlyBold.send,
+                  color: AppAssets.colors.light,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
