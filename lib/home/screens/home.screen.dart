92413d768466efdda5f93529c4ca813632b1be61
Aiken Tine Ahac <ahac.aiken@gmail.com>
2023-02-03 09:34:20 +0100

    Display message when user is not in a cult

--- lib/home/screens/home.screen.dart
+++ lib/home/screens/home.screen.dart
@@ -6,6 +6,7 @@ import 'package:app/home/utils/post.data.dart';
 import 'package:app/home/widgets/feed_text.widget.dart';
 import 'package:app/home/widgets/header.widget.dart';
 import 'package:app/home/widgets/post/image_post.widget.dart';
+import 'package:app/home/widgets/post/posts.widget.dart';
 import 'package:app/home/widgets/post/text_post.widget.dart';
 import 'package:app/profile/api/models/profile_picture.response.dart';
 import 'package:app/profile/providers/me.provider.dart';
@@ -26,84 +27,37 @@ class HomeScreen extends ConsumerWidget {
     return pfp.url;
   }
 
-  Widget separator() {
-    return const SizedBox(height: 15.0);
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final height = MediaQuery.of(context).size.height;
+    final p = ref.watch(meProvider);
 
-    final pr = ref.watch(meProvider);
-    final po = ref.watch(asyncPostsProvider);
-
-    void votePost(String id, String type) {
-      ref.read(asyncPostsProvider.notifier).votePost(id, type);
-    }
-
-    void openPost(String id) {
-      Navigator.pushReplacementNamed(
-        context,
-        PostScreen.routeName,
-        arguments: Args(id),
-      );
-    }
-
-    return pr.when(
+    return p.when(
       loading: () => const LoadingScreen(),
       error: (err, stack) => ErrorScreen(error: err),
-      data: (profile) => po.when(
-        loading: () => const LoadingScreen(),
-        error: (err, stack) => ErrorScreen(error: err),
-        data: (posts) => Scaffold(
-          backgroundColor: AppAssets.colors.dark,
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 40.0),
-              Header(
-                pfp: assignProfilePicture(profile.profilePicture),
-                name: '${profile.firstName} ${profile.lastName}',
-                cult: '${profile.cult?.cult?.name}',
-                role: '${profile.cult?.role}',
-              ),
-              const FeedText(),
-              SizedBox(
-                height: height * 0.75,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: ListView.separated(
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (context, i) {
-                      final data = posts[i];
-                      final upvoted = ref
-                          .watch(asyncPostsProvider.notifier)
-                          .isUpvoted(data.votes);
-                      final downvoted = ref
-                          .watch(asyncPostsProvider.notifier)
-                          .isDownvoted(data.votes);
-
-                      final post = PostData(
-                        data: data,
-                        upvoted: upvoted,
-                        downvoted: downvoted,
-                        vote: votePost,
-                      );
-
-                      return GestureDetector(
-                        onTap: () => openPost(post.data.id),
-                        child: post.data.type == "Image"
-                            ? ImagePost(post: post)
-                            : TextPost(post: post),
-                      );
-                    },
-                    separatorBuilder: (c, i) => separator(),
-                    itemCount: posts.length,
+      data: (profile) => Scaffold(
+        backgroundColor: AppAssets.colors.dark,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 40.0),
+            Header(
+              pfp: assignProfilePicture(profile.profilePicture),
+              name: '${profile.firstName} ${profile.lastName}',
+              cult: '${profile.cult?.cult?.name}',
+              role: '${profile.cult?.role}',
+            ),
+            const FeedText(),
+            profile.cult != null
+                ? const PostsWidget()
+                : Center(
+                    child: Text(
+                      'You are not in a cult',
+                      style: TextStyle(
+                        color: AppAssets.colors.light,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ],
-          ),
+          ],
         ),
       ),
     );
--- lib/home/widgets/post/posts.widget.dart
+++ lib/home/widgets/post/posts.widget.dart
@@ -1,3 +1,10 @@
+import 'package:app/common/screens/error.screen.dart';
+import 'package:app/common/screens/loading.screen.dart';
+import 'package:app/home/providers/posts.provider.dart';
+import 'package:app/home/screens/post.screen.dart';
+import 'package:app/home/utils/post.data.dart';
+import 'package:app/home/widgets/post/image_post.widget.dart';
+import 'package:app/home/widgets/post/text_post.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +13,62 @@ class PostsWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Placeholder();
+    final height = MediaQuery.of(context).size.height;
+
+    final p = ref.watch(asyncPostsProvider);
+
+    void votePost(String id, String type) {
+      ref.read(asyncPostsProvider.notifier).votePost(id, type);
+    }
+
+    void openPost(String id) {
+      Navigator.pushReplacementNamed(
+        context,
+        PostScreen.routeName,
+        arguments: Args(id),
+      );
+    }
+
+    Widget separator() {
+      return const SizedBox(height: 15.0);
+    }
+
+    return p.when(
+      loading: () => const LoadingScreen(),
+      error: (err, stack) => ErrorScreen(error: err),
+      data: (posts) => SizedBox(
+        height: height * 0.75,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, i) {
+              final data = posts[i];
+              final upvoted =
+                  ref.watch(asyncPostsProvider.notifier).isUpvoted(data.votes);
+              final downvoted = ref
+                  .watch(asyncPostsProvider.notifier)
+                  .isDownvoted(data.votes);
+
+              final post = PostData(
+                data: data,
+                upvoted: upvoted,
+                downvoted: downvoted,
+                vote: votePost,
+              );
+
+              return GestureDetector(
+                onTap: () => openPost(post.data.id),
+                child: post.data.type == "Image"
+                    ? ImagePost(post: post)
+                    : TextPost(post: post),
+              );
+            },
+            separatorBuilder: (c, i) => separator(),
+            itemCount: posts.length,
+          ),
+        ),
+      ),
+    );
   }
 }
